29539194c6f7152cdee9d73b5ee0ccb800b2280e
Mahammad Agayev <53854177+MahammadAgayev@users.noreply.github.com>
2025-02-28 15:52:38 +0100

    Update copilot.lua

--- nvim/lua/aghayev/lazy/copilot.lua
+++ nvim/lua/aghayev/lazy/copilot.lua
@@ -5,7 +5,21 @@ return {
   config = function()
     require("copilot").setup({
             suggestion = { enabled = false },
-            panel = { enabled = false },
+             panel = {
+                enabled = false,
+                auto_refresh = false,
+                keymap = {
+                  jump_prev = "<C-u>",
+                  jump_next = "<C-d>",
+                  accept = "<C-y>",
+                  refresh = "gr",
+                  open = "<M-CR>"
+               },
+               layout = {
+                 position = "right", -- | top | left | right | horizontal | vertical
+                 ratio = 0.4
+               },
+            }
         })
   end,
 }
