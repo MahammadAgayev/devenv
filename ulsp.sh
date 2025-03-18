3ba64785d18c05c53524312e0dc4c2907e5f30da
Mahammad Aghayev <53854177+MahammadAgayev@users.noreply.github.com>
2025-03-18 16:30:12 +0100

    Create ulsp.sh

---
+++ ulsp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+#go
+monorepo="home/user/go-code"  # Replace with your monorepo path
+#java
+#monorepo="home/user/fievel"
+
+cd "$monorepo" || exit 1
+while true; do
+  # on devpod, use ULSP_ENVIRONMENT=devpod
+  UBER_CONFIG_DIR="$monorepo/tools/ide/ulsp/config" ULSP_ENVIRONMENT=local uexec "$monorepo/tools/ide/ulsp/ulsp-daemon" || true
+  sleep 5
+done
