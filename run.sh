038c400b49fdf203ce47fff2938cbd79f4223091
Mahammad Aghayev <Mahammad Aghayev>
2025-06-01 13:06:48 +0200

    Adds vault pass to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/MahammadAgayev/devenv/refs/heads/main/configure.yml -o configure.yml && ansible-playbook configure.yml
+curl https://raw.githubusercontent.com/MahammadAgayev/devenv/refs/heads/main/configure.yml -o configure.yml && ansible-playbook configure.yml --ask-vault-pass --ask-become-pass
