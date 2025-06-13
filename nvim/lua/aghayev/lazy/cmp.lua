ec662f4062d608744bf75b3c390d4f0758ec84ba
Mahammad Aghayev <53854177+MahammadAgayev@users.noreply.github.com>
2025-06-13 12:16:46 +0200

    Update cmp.lua

--- nvim/lua/aghayev/lazy/cmp.lua
+++ nvim/lua/aghayev/lazy/cmp.lua
@@ -26,8 +26,10 @@ return {
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
         { name = "avante" },
