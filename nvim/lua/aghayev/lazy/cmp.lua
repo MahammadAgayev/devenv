ca9dc47f77ea2b81e794b222ab4aa1499f7d33dd
Mahammad Aghayev <53854177+MahammadAgayev@users.noreply.github.com>
2025-06-13 11:58:35 +0200

    Create cmp.lua

---
+++ nvim/lua/aghayev/lazy/cmp.lua
@@ -0,0 +1,64 @@
+return {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'onsails/lspkind-nvim',
+    'zbirenbaum/copilot.lua',
+    'zbirenbaum/copilot-cmp',
+  },
+  config = function()
+    -- Setup Copilot (disable inline suggestion/panel)
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+    require("copilot_cmp").setup()
+
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+
+    cmp.setup({
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",      -- icon + text
+          maxwidth = 50,
+          ellipsis_char = "...",
+          symbol_map = { Copilot = "" },
+        }),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "copilot" },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+
+    -- Cmdline completion
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" }
+      }, {
+        { name = "cmdline" }
+      }),
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" }
+      },
+    })
+  end,
+}
